994bdeb2b096933c3b6804cb8d6a097d5c3b4149
Ertugrul Akdag <ertugrulakdag@gmail.com>
2025-09-15 22:38:44 +0300

    fix

--- ERT.MqttHost/Program.cs
+++ ERT.MqttHost/Program.cs
@@ -1,8 +1,19 @@
 using ERT.MqttHost;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<BrokerWorker>();
+
+builder.Services.AddWindowsService(o => o.ServiceName = "ERT MQTT Host");
+
+builder.Logging.ClearProviders();
+
+builder.Logging.AddEventLog(opt =>
+{
+    opt.SourceName = "ERT.MqttHost";
+    opt.LogName = "Application";
+});
+
 builder.Services.Configure<MqttOptions>(builder.Configuration.GetSection("MqttBroker"));
+builder.Services.AddHostedService<BrokerWorker>();
 
 var host = builder.Build();
 host.Run();
